a25a658299eb875d692a79c09181fe59c92d9f82
Danish-Hasan <danhas04440@gmail.com>
2026-06-02 15:13:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ def generate_order_id(worksheet):
 
 @app.route("/")
 def index():
-    return send_from_directory(os.path.join(BASE_DIR, "static"), "index.html")
+    app = Flask(__name__, static_folder=os.path.join(BASE_DIR, "static"))
 
 
 @app.route("/submit", methods=["POST"])
